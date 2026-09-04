da41ba970d1fec1b6e8ec06e6807a70e52154820
ys6573-dot <ys6573@srmist.edu.in>
2026-09-04 19:53:55 +0530

    Added MaxProfit.java

---
+++ MergeArrays.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class MergeArrays {
+    public static int[] mergeSortedArrays(int[] arr1, int[] arr2) {
+        int i = 0, j = 0, k = 0;
+        int[] result = new int[arr1.length + arr2.length];
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] <= arr2[j]) {
+                result[k++] = arr1[i++];
+            } else {
+                result[k++] = arr2[j++];
+            }
+        }
+        while (i < arr1.length) result[k++] = arr1[i++];
+        while (j < arr2.length) result[k++] = arr2[j++];
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5};
+        int[] arr2 = {2, 4, 6};
+        System.out.println(Arrays.toString(mergeSortedArrays(arr1, arr2)));
+    }
+}
