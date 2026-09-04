f545dbb4e6b1306bbce3bdeb1daf69582a9ea3d6
ys6573-dot <ys6573@srmist.edu.in>
2026-09-04 19:22:04 +0530

    MaxProfit java program

---
+++ Max.java
@@ -0,0 +1,19 @@
+public class MaxProfit {
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices)); // 5
+    }
+}
