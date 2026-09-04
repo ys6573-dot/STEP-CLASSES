a6b2a040d01175359af55d5b5e9b64c00b4da2be
ys6573-dot <ys6573@srmist.edu.in>
2026-09-04 20:18:57 +0530

    Added MaxProfit.java

---
+++ FindMinRotatedArray.java
@@ -0,0 +1,20 @@
+public class FindMinRotatedArray {
+    public static int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1; // min is in right half
+            } else {
+                right = mid; // min is in left half (including mid)
+            }
+        }
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(findMin(nums)); // 0
+    }
+}
