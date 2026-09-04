84c84065ae4fd22d7c2600437ec198cba432b83d
ys6573-dot <ys6573@srmist.edu.in>
2026-09-04 20:09:18 +0530

    Added MaxProfit.java

---
+++ MaxSubArray.java
@@ -0,0 +1,17 @@
+public class MaxSubArray {
+    public static int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(nums)); // 6
+    }
+}
