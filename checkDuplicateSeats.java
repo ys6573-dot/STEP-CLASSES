896b8a3bc1319c728f6564e3f5822d9fbb4afcfe
ys6573-dot <ys6573@srmist.edu.in>
2026-09-04 12:28:19 +0530

    seat duplication checker java program

---
+++ checkDuplicateSeats.java
@@ -0,0 +1,21 @@
+public class SeatDuplicationChecker {
+
+    public static void checkDuplicateSeats(int[] seatNumbers) {
+        boolean duplicateFound = false;
+        for (int i = 0; i < seatNumbers.length; i++) {
+            for (int j = i + 1; j < seatNumbers.length; j++) {
+                if (seatNumbers[i] == seatNumbers[j]) {
+                    System.out.println("Duplicate Seat Number Found: " + seatNumbers[i]);
+                    duplicateFound = true;
+                }
+            }
+        }
+        if (!duplicateFound) {
+            System.out.println("No Duplicate Seats Found");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] seats1 = {101, 102, 103, 102, 105};
+        int[] seats2 = {101, 102, 103, 104, 105};
+
