b6f9054be773660fee5c338225fa770b02966491
ys6573-dot <ys6573@srmist.edu.in>
2026-09-11 19:39:12 +0530

    duplicate team finder java program

---
+++ DuplicateTeamFinder.java
@@ -0,0 +1,17 @@
+public class DuplicateTeamFinder {
+    static String findDuplicateTeam(String[] teamNames) {
+        for (int i = 0; i < teamNames.length; i++) {
+            for (int j = i + 1; j < teamNames.length; j++) {
+                if (teamNames[i].equals(teamNames[j])) {
+                    return "Duplicate Found: " + teamNames[i];
+                }
+            }
+        }
+        return "No Duplicates Found";
+    }
+
+    public static void main(String[] args) {
+        String[] teams = {"ByteForce", "CodeCrafters", "ByteForce"};
+        System.out.println(findDuplicateTeam(teams)); // Duplicate Found: ByteForce
+    }
+}
