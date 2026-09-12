f29490f7c3a402560bce11fbadd2089730d265ad
ys6573-dot <ys6573@srmist.edu.in>
2026-09-12 11:56:09 +0530

    payroll account java program

---
+++ PayrollAccount.java
@@ -0,0 +1,43 @@
+class PayrollAccount {
+    private double basicSalary;
+    private double bonus;
+
+    public PayrollAccount(double basicSalary) {
+        if (basicSalary < 0) {
+            System.out.println("Warning: Negative salary, set to 0.");
+            this.basicSalary = 0;
+        } else {
+            this.basicSalary = basicSalary;
+        }
+        this.bonus = 0;
+    }
+
+    public void creditBonus(double amount) {
+        if (amount <= 0) {
+            System.out.println("Bonus rejected: must be positive.");
+        } else {
+            bonus += amount;
+            System.out.println("Bonus credited: Rs " + amount);
+        }
+    }
+
+    public void deductTax(double percent) {
+        if (percent < 0 || percent > 100) {
+            System.out.println("Invalid tax percent.");
+        } else {
+            basicSalary -= basicSalary * (percent / 100.0);
+            System.out.println("Tax deducted: " + percent + "%");
+        }
+    }
+
+    public double getNetSalary() {
+        return basicSalary + bonus;
+    }
+
+    public static void main(String[] args) {
+        PayrollAccount account = new PayrollAccount(50000);
+        account.creditBonus(5000);
+        account.deductTax(10);
+        System.out.println("Net salary: Rs " + account.getNetSalary());
+    }
+}
