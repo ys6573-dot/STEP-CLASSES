7a37069ddea57bf53a21c1c45ace087d159c78f2
ys6573-dot <ys6573@srmist.edu.in>
2026-09-12 11:15:24 +0530

    student java program

---
+++ Student.java
@@ -0,0 +1,24 @@
+class Student {
+    String name;
+    int attendance;
+    static String collegeName = "SRM Institute of Science and Technology";
+    static int studentCount = 0;
+
+    Student(String name, int attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    static void printCollegeInfo() {
+        System.out.println(collegeName);
+        System.out.println("Students created: " + studentCount);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Ravi", 85);
+        Student s2 = new Student("Anitha", 90);
+
+        Student.printCollegeInfo(); // called via class name
+    }
+}
