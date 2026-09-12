2040bfc845b7770d7bd979bc5ba25aff5f484a63
ys6573-dot <ys6573@srmist.edu.in>
2026-09-12 11:58:30 +0530

    employee java program

---
+++ Employee.java
@@ -0,0 +1,30 @@
+class Employee {
+    String empId;
+    String empName;
+    double salary;
+    boolean isIntern;
+
+    public Employee(String empId, String empName, double salary) {
+        this.empId = empId;
+        this.empName = empName;
+        this.salary = salary;
+        this.isIntern = false;
+    }
+
+    public Employee(String empId, String empName) {
+        this(empId, empName, 0); // chain to 3-arg constructor
+        this.isIntern = true;
+    }
+
+    void printProfile() {
+        System.out.println(empId + " | " + empName + " | Rs " + salary + " | Intern: " + isIntern);
+    }
+
+    public static void main(String[] args) {
+        Employee permanent = new Employee("E-101", "Divya", 65000);
+        Employee intern = new Employee("E-102", "Arjun");
+
+        permanent.printProfile();
+        intern.printProfile();
+    }
+}
