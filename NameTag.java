f334428cfec757e4f2c18f7b3d2f108c3c1f22c3
ys6573-dot <ys6573@srmist.edu.in>
2026-09-19 10:42:04 +0530

    NameTag java program

---
+++ NameTag.java
@@ -0,0 +1,20 @@
+// Problem 3: The Nickname Tag
+public class NameTag {
+    private final String firstName;
+    private final String lastName;
+
+    public NameTag(String fullName) {
+        String[] parts = fullName.split(" ");
+        this.firstName = parts[0];
+        this.lastName = parts[1];
+    }
+
+    public String getNickname() {
+        return firstName + " " + lastName.charAt(0) + ".";
+    }
+
+    public static void main(String[] args) {
+        NameTag tag = new NameTag("Maria Gomez");
+        System.out.println("Nickname: " + tag.getNickname());
+    }
+}
